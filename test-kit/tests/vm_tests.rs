5c4a69e5137387c720d2154cd273c14629216b0a
Alexander Koz <a@koz.email>
2020-09-25 15:11:13 +0300

    add tests for multimodule compile & publish

--- test-kit/tests/vm_tests.rs
+++ test-kit/tests/vm_tests.rs
@@ -1,3 +1,4 @@
+use compiled_protos::vm_grpc::VmExecuteResponse;
 use libra::{prelude::*, lcs};
 use dvm_test_kit::*;
 use dvm_test_kit::compiled_protos::vm_grpc::{VmArgs, VmTypeTag};
@@ -165,4 +166,89 @@ fn test_update_std_module_1() {
     );
     test_kit.assert_success(&res);
     test_kit.merge_result(&res);
+}
+
+#[test]
+fn test_publish_one_module() {
+    let test_kit = TestKit::new();
+    test_kit.add_std_module(include_str!("resources/store.move"));
+
+    let sources = [
+        "address 0x1 { module Foo { public fun foo(): u64 { 1 } } }",
+        "module Bar { public fun bar(): u64 { 2 } }",
+    ];
+    sources.iter().for_each(|src| {
+        let res = test_kit.publish_module(src, gas_meta(), CORE_CODE_ADDRESS);
+        test_kit.assert_success(&res);
+        test_kit.merge_result(&res);
+    });
+
+    let res = store_call_tx_without_args(&test_kit, "use 0x1::Foo;", "Foo::foo();");
+    let value: U64Store = lcs::from_bytes(&res.write_set[0].value).unwrap();
+    assert_eq!(value.val, 1);
+
+    let res = store_call_tx_without_args(&test_kit, "use 0x1::Bar;", "Bar::bar();");
+    let value: U64Store = lcs::from_bytes(&res.write_set[0].value).unwrap();
+    assert_eq!(value.val, 2);
+}
+
+#[test]
+fn test_publish_multi_module() {
+    let test_kit = TestKit::new();
+    test_kit.add_std_module(include_str!("resources/store.move"));
+
+    let source = r#"
+        address 0x1 {
+            module Foo { public fun foo(): u64 { 1 } }
+            module Bar { public fun bar(): u64 { 2 } }
+            module Boo { public fun boo(): u64 { 10 } }
+        }
+        "#;
+
+    let res = test_kit.publish_module(&source, gas_meta(), CORE_CODE_ADDRESS);
+    test_kit.assert_success(&res);
+    test_kit.merge_result(&res);
+
+    let res = store_call_tx_without_args(&test_kit, "use 0x1::Boo;", "Boo::boo();");
+    let value: U64Store = lcs::from_bytes(&res.write_set[0].value).unwrap();
+    assert_eq!(value.val, 4);
+
+    let res = store_call_tx_without_args(
+        &test_kit,
+        "use 0x1::Foo; use 0x1::Bar;",
+        "Foo::foo() + Bar::bar();",
+    );
+    let value: U64Store = lcs::from_bytes(&res.write_set[0].value).unwrap();
+    assert_eq!(value.val, 1 + 2 + 10);
+}
+
+#[inline]
+fn store_call_tx_without_args<S: AsRef<str>>(
+    kit: &TestKit,
+    using: S,
+    call: S,
+) -> VmExecuteResponse {
+    let source_tx = format!(
+        r#"
+        script {{
+            {using}
+            use 0x1::Store;
+            fun main(account: &signer) {{
+                let res = {call}
+                Store::store_u64(account, res);
+            }}
+        }}
+        "#,
+        using = using.as_ref(),
+        call = call.as_ref()
+    );
+    let res = kit.execute_script(
+        &source_tx,
+        gas_meta(),
+        vec![],
+        vec![],
+        vec![AccountAddress::random()],
+    );
+    kit.assert_success(&res);
+    res
 }
